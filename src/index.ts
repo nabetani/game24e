cba486a710087825e7ef4364766ead875376eaa3
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-05-18 21:05:17 +0900

    32✕32

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({antialias: true})
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -19,11 +19,18 @@ window.addEventListener('resize', () => {
 
 new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshNormalMaterial({ wireframe: true })
+for( let i=0 ; i<1024 ; ++i ){
+  const geometry = new THREE.BoxGeometry()
+  const material = new THREE.MeshNormalMaterial({ wireframe: false })
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+  const cube = new THREE.Mesh(geometry, material)
+  cube.applyMatrix4( (new THREE.Matrix4()).multiplyScalar(0.3) );
+  const x = ( (i & 31) - 15 );
+  const z = ( (i >>5) - 15 );
+  cube.translateX(x*0.35);
+  cube.translateZ(z*0.35);
+  scene.add(cube)
+}
 
 function animate() {
   requestAnimationFrame(animate)
