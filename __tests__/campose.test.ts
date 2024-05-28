ab99fa2d5869bceb9c7d78aa1e47eebbc50f5f30
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-05-28 23:31:40 +0900

    回転内分点を実装

--- __tests__/campose.test.ts
+++ __tests__/campose.test.ts
@@ -1,30 +1,21 @@
 // copy from https://sbcode.net/threejs/renderer/
 import * as T from 'three'
-import Stats from 'three/addons/libs/stats.module.js'
-import *  as W from '../src/world'
+import * as C from '../src/calc'
 
-const newQA = (x: number, y: number, z: number, angle: number): T.Quaternion => {
-    const d = 1 / ((x ** 2 + y ** 2 + z ** 2) ** 0.5)
-    return new T.Quaternion().setFromAxisAngle({ x: x * d, y: y * d, z: z * d }, angle * Math.PI / 180)
-}
+test("interVec.1", () => {
+    const p0 = new T.Vector3(1, 0, 0)
+    const p1 = new T.Vector3(0, 1, 0)
+    const i = C.interVec(p0, p1, 0.2)
+    expect(i.x).toBeCloseTo(Math.cos(Math.PI * 0.1), 0.01)
+    expect(i.y).toBeCloseTo(Math.sin(Math.PI * 0.1), 0.01)
+    expect(i.z).toBeCloseTo(0, 0.01)
+});
 
-
-const camSetRotFromQ = (cam: T.Camera, q: T.Quaternion) => {
-    cam.setRotationFromEuler(new T.Euler().setFromQuaternion(q))
-}
-
-const camWorldDir = (cam: T.Camera): T.Vector3 => {
-    const { x, y, z } = cam.getWorldDirection(new T.Vector3())
-    const n = 1000
-    const r = (i: number): number => 0 + Math.round(i * n) / n
-    return new T.Vector3(r(x), r(y), r(z))
-}
-
-test("simpleQ", () => {
-    const q = newQA(1, 0, 0, 0).multiply(newQA(0, 1, 0, -90))
-    console.log(`q:${q.toJSON()}`)
-    const cam = new T.PerspectiveCamera()
-    camSetRotFromQ(cam, q)
-    const o = camWorldDir(cam)
-    expect(o.toArray()).toStrictEqual([1, 0, 0])
+test("interVec.2", () => {
+    const p0 = new T.Vector3(1, 0, 0)
+    const p1 = new T.Vector3(0, 0, -1)
+    const i = C.interVec(p0, p1, 0.4)
+    expect(i.x).toBeCloseTo(Math.cos(Math.PI * 0.2), 0.01)
+    expect(i.y).toBeCloseTo(0, 0.01)
+    expect(i.z).toBeCloseTo(-Math.sin(Math.PI * 0.2), 0.01)
 });
--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,5 @@
+import * as T from 'three'
+
 export const sincos = (a: number, b: number | null = null): { sin: number, cos: number } => {
   const t = b === null ? a : (Math.PI * 2 * a / b)
   return { sin: Math.sin(t), cos: Math.cos(t) }
@@ -19,3 +21,12 @@ export const clamp = (v: number, low: number, high: number): number => {
   return v
 }
 
+export const interVec = (a0: T.Vector3Like, b0: T.Vector3Like, r: number): T.Vector3 => {
+  const a = new T.Vector3(a0.x, a0.y, a0.z)
+  const b = new T.Vector3(b0.x, b0.y, b0.z)
+  const ang = a.angleTo(b)
+  const c = a.clone().cross(b)
+  const r0 = a.clone().applyQuaternion(new T.Quaternion().setFromAxisAngle(c, ang * r))
+  const len = (a.length() * (1 - r) + b.length() * r)
+  return r0.setLength(len)
+}
